447c266dc93a1c66a777875cbc05bd2e5cd2827a
Kelly Kessler <ksquaredkelly@gmail.com>
2021-08-27 11:43:16 -0500

    lists all places, generates new list of place names starting with -the-

---
+++ place-names/js/placeNames/main.js
@@ -0,0 +1,16 @@
+const names = [
+  "Nashville", "Hong Kong", "The back yard", "Earth",
+  "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+];
+
+console.log("All place names:");
+
+for (const name of names) {
+  console.log(name);
+}
+
+const theNames = names.filter(name => name.includes("The"))
+
+for (const name of theNames) {
+  console.log(name)
+}
